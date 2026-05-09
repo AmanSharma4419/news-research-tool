282036ee2622d0b42814f6bac42064d22a0749ad
AmanSharma4419 <sharmaaman4419@gmail.com>
2026-05-10 00:41:36 +0530

    minor improvements in meta data storing

--- app/services/loader_service.py
+++ app/services/loader_service.py
@@ -1,34 +1,38 @@
-from trafilatura import fetch_url, extract
+from trafilatura import fetch_url, extract, extract_metadata
 from langsmith import traceable
+
 
 @traceable(name="load_article")
 def load_article(url: str):
 
     try:
-
         downloaded = fetch_url(url)
 
         if not downloaded:
             raise Exception("Failed to download article")
 
+        metadata = extract_metadata(downloaded)
+
         content = extract(
             downloaded,
             include_comments=False,
-            include_tables=False
+            include_tables=False,
+            include_links=False,
+            include_images=False,
+            favor_precision=True,
+            deduplicate=True
         )
 
         if not content:
             raise Exception("Failed to extract article content")
 
-    
-
         return {
-            "title": "News Article",
+            "title": metadata.title if metadata else "News Article",
             "content": content,
-            "source_url": url
+            "source_url": url,
         }
-    except Exception as e:
 
+    except Exception as e:
         raise Exception(
             f"Failed to process URL: {url} | Error: {str(e)}"
         )
--- app/services/retrieval_service.py
+++ app/services/retrieval_service.py
@@ -1,23 +1,31 @@
-from app.services.vector_store_service import get_vector_store
 from langsmith import traceable
 
-@traceable(name="retrieve_documents")
-def retrieve_documents(query:str):
-    vector_store = get_vector_store()
-    
-    results =  vector_store.similarity_search_with_score(query=query, k=6)
-    
-    formatted_results = []
-    
-    for doc,score in results:
-        formatted_results.append(
-            {
-            "content": doc.page_content,
-            "metadata": doc.metadata,
-            "score": score
-            }
-        )
-        
-    return formatted_results
+from app.services.vector_store_service import get_vector_store
 
-    
+
+@traceable(name="retrieve_documents")
+def retrieve_documents(query: str, limit: int = 5):
+
+    vector_store = get_vector_store()
+
+    results = vector_store.similarity_search_with_score(
+        query=query,
+        k=limit
+    )
+
+    MIN_SCORE = 0.5
+
+    formatted_results = []
+
+    for document, score in results:
+
+        if score < MIN_SCORE:
+            continue
+
+        formatted_results.append({
+            "content": document.page_content,
+            "metadata": document.metadata,
+            "score": score
+        })
+
+    return formatted_results
